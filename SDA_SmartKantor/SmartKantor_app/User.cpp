233bf01c7d905bf1dfd1815187c236892a989b67
Patrycja Z <patrycja.zet3@gmail.com>
2022-10-22 01:08:20 +0200

    User uzywa klasy BalanceManager

--- SDA_SmartKantor/SmartKantor_app/User.cpp
+++ SDA_SmartKantor/SmartKantor_app/User.cpp
@@ -7,14 +7,27 @@
 
 float User::buy(Currency::CurrencyCode currCodeSource, float amount)
 {
-    float value = converter1.calculateBuy(amount, currCodeSource);
-    return value;
+    // kupujemy od uzytkownika np. eur
+    // zwieksza nam sie euro w balansie, wyplacamy pln
+    float value_pln = converter1.calculateBuy(amount, currCodeSource);
+    
+    _balance.deposit(currCodeSource, amount);
+    _balance.withdraw(Currency::CurrencyCode::PLN, value_pln);
+
+    return value_pln;
 }
 
 float User::sell(Currency::CurrencyCode currCodeSource, float amount) 
 {
+    // sprzedajemy euro
+    // przybywa pln, ubywa euro
     float value = converter1.calculateSell(amount, currCodeSource);
+
+    _balance.deposit(Currency::CurrencyCode::PLN, value);
+    _balance.withdraw(currCodeSource, amount);
+
     return value;
+    
 }
 
 std::map<Currency::CurrencyCode, Currency>& User::getRates()
--- SDA_SmartKantor/SmartKantor_app/User.hpp
+++ SDA_SmartKantor/SmartKantor_app/User.hpp
@@ -4,18 +4,23 @@
 
 #include "TempRatesRetriever.h"
 #include "Converter.h"
+#include "BalanceManager.hpp"
 
 class User
 { 
+private:
 	Converter converter1;
+	BalanceManager _balance;
 	TempRatesRetriever& _tempRates;
 
 public:
 	User(TempRatesRetriever& temp)
 		: _tempRates(temp) {}
 
+	//kantor kupuje walute
 	float buy(Currency::CurrencyCode currCodeSource, float amount);
 
+	//kantor sprzedaje walute
 	float sell(Currency::CurrencyCode currCodeSource, float amount);
 
 	std::map<Currency::CurrencyCode, Currency>& getRates();
