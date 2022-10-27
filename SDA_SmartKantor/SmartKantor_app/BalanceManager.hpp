c4308a73c18c1ccbabb51ea067772426c2a7447d
Kinga Jablonska <k.a.jablonska93@gmail.com>
2022-10-27 20:18:05 +0200

    Update BalanceManagera o rzucenie wyjatku

--- SDA_SmartKantor/SmartKantor_app/BalanceManager.hpp
+++ SDA_SmartKantor/SmartKantor_app/BalanceManager.hpp
@@ -32,25 +32,4 @@ public:
 
 
 	
-	//bool withdraw (Currency::CurrencyCode currCodeSource, float amount)
-	//{
-	//	std::map<Currency::CurrencyCode, txnAmount> rates;
-	//	float wynik = amount * rates[currCodeSource].getBuyPrice();
-	//	return wynik;
-
-	////atrybuty
-	//float depositAmount = 500.00;//ile mamy obcej waluty
-	////float amount = 300.25; //ile chcemy kupiæ/sprzedaæ obcej waluty
-	//float withdrawalAmount; //kwota po przeliczeniu jaka musimy wydaæ z kasy
-	//float incomingAmount; //kwota jaka przyjmujemy do kasy w walucie od klienta
-
-	//std::string command; //buy lub sell
-	//std::string currency = "EUR"; //jaka to waluta
-
-	////metody
-	//bool withdraw(float amount, float depositAmount);
-	//void deposit(float amount, float depositAmount);
-
-	//metody, ktore aktualizuja balance, odejmuja (withdraw) i dodaja (deposit)
-	//pobieraja dane z plikow json, dodaja lub odejmuja te kwote, ktora klient chce wymienic i znow zapisuja dane do json
-	//przy withdraw jest bool, bo dobrze jakby sprawdzilo czy jest tyle kasy by odjac
+	
